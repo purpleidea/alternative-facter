bb049675e6bab8011732f9df4f9548f2ac64d0a6
James <james@shubin.ca>
2017-02-06 06:29:27 -0500

    alternative fact: presidential_hand_size
    Nibalizer insisted I send a patch. I don't understand why hand size was always an issue, but here's the fact.

---
+++ lib/facter/presidential_hand_size.rb
@@ -0,0 +1,11 @@
+# Fact: presidential_hand_size
+#
+# Purpose: Declares the presidential hand size for all to know.
+# Caveats: Liberal media often disputes these statements.
+
+#Facter.add(:presidential_hand_size) do
+#  "Tiny" # not that this matters
+#end
+Facter.add(:presidential_hand_size) do
+  "Very large"
+end
